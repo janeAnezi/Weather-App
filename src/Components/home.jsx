ba032006a1af5c95d3c88493dd508bd8cd499690
Anezijane <ahnnieanezi@gmail.com>
2024-03-28 08:49:01 +0100

    add style to home

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,13 +1,13 @@
 export default function Home() {
     return (
         <>
-            <div className="bg-gradient-to-r from-sky-500 to-indigo-500 ">
+            <div className="bg-gradient-to-r from-sky-500 to-indigo-500 p-4">
                 <nav className="text-white">City / Country</nav>
                 <div className="flex justify-center items-center">
                     <div className="inline-block w-80">
                         <img src="./src/assets/images/cloud1.png" alt="cloud image" />
-                        <img src="./src/assets/images/cloud2.png" alt="cloud image" />
-                        <h1>WEATHER FORECASTS</h1>
+                        <img className="ml-12 pt-8" src="./src/assets/images/cloud2.png" alt="cloud image" />
+                        <h1 className="text-white font-bold text-5xl text-center">WEATHER FORECASTS</h1>
                         <img src="./src/assets/images/cloud1.png" alt="cloud image" />
                         <p>What is the current weather condition in your city?</p>
                         <button>GET STARTED</button>
